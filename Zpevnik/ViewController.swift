983294f08bcde49aaf6579c885649de25afe8c1f
Patrik <patrikdobidobias@gmail.com>
2019-07-23 11:21:25 +0200

    Initial Commit

---
+++ Zpevnik/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Zpevnik
+//
+//  Created by Patrik Dobiáš on 23/07/2019.
+//  Copyright © 2019 Patrik Dobiáš. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
